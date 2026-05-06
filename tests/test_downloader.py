b352e2553b2ead31c97ff8459ac42867726c4e28
iniad <cornforprogramming@gmail.com>
2026-05-06 14:33:15 +0900

    実行コマンドのメモを追加

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -2,7 +2,8 @@ import os
 
 from core.downloader import download
 
-
+# 実行コマンド
+# .venv\\Scripts\\pytest.exe tests/test_downloader.py -v
 def test_download():
     url = "https://www.youtube.com/watch?v=BXWGrp0XXNo"
     filepath, title = download(url)
